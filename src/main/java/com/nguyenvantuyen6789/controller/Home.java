d625c96514f02181c867978fa19c1b1e70763994
Admin <nguyenvantuyen6789@gmail.com>
2021-09-28 17:29:03 +0700

    Xong! Show Message Qua Model

--- src/main/java/com/nguyenvantuyen6789/controller/Home.java
+++ src/main/java/com/nguyenvantuyen6789/controller/Home.java
@@ -1,13 +1,15 @@
 package com.nguyenvantuyen6789.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Home {
 
     @RequestMapping("home")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("message", "Success!");
         return "trang-chu";
     }
 }
