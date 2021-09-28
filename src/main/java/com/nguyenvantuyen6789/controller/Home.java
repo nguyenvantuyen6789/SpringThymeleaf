a9c340b6432ee0aed90a9412576b2352e8b9aa81
Admin <nguyenvantuyen6789@gmail.com>
2021-09-28 17:35:43 +0700

    Xong! Show List Person Ra Màn Hình

--- src/main/java/com/nguyenvantuyen6789/controller/Home.java
+++ src/main/java/com/nguyenvantuyen6789/controller/Home.java
@@ -1,8 +1,12 @@
 package com.nguyenvantuyen6789.controller;
 
+import com.nguyenvantuyen6789.entity.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class Home {
@@ -10,6 +14,14 @@ public class Home {
     @RequestMapping("home")
     public String home(Model model) {
         model.addAttribute("message", "Success!");
+
+        List<Person> listPerson = new ArrayList<>();
+        listPerson.add(new Person(1, "Tuyên Nguyễn"));
+        listPerson.add(new Person(2, "Hùng Nguyễn"));
+        listPerson.add(new Person(3, "Ngọc Nguyễn"));
+
+        model.addAttribute("listPerson", listPerson);
+
         return "trang-chu";
     }
 }
---
+++ src/main/java/com/nguyenvantuyen6789/entity/Person.java
@@ -0,0 +1,15 @@
+package com.nguyenvantuyen6789.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Person {
+
+    private int id;
+    private String name;
+
+}
